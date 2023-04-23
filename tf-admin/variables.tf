1e0ddb03258c533e5f6b64821a70cfa63ed1232e
Miles <miles.harrison@hashicorp.com>
2023-04-23 16:52:45 -0500

    Adding initial workspaces and related resources

--- tf-admin/variables.tf
+++ tf-admin/variables.tf
@@ -5,7 +5,7 @@ variable "tfc_organization" {
 }
 
 variable "workspace_names" {
-    type = set
+    type = set(string)
     description = "List of TFC Workspace Names to create"
     default = [
         "tf-hcp-vault", 
