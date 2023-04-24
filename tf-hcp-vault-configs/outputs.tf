8f56196d8dbf7aaed2d18cd484911bff3bc1bbeb
Miles <miles.harrison@hashicorp.com>
2023-04-23 23:01:16 -0500

    modifying AWS IAM resources

--- tf-hcp-vault-configs/outputs.tf
+++ tf-hcp-vault-configs/outputs.tf
@@ -3,20 +3,20 @@ output "vault_jwt_role" {
   value       = module.jwt_auth.vault_role_jwt_auth
 }
 
-output "vault_target_iam_role_arn" {
-  description = "The AWS IAM role ARN for the Vault assume role credential type."
-  value       = module.aws_secrets.vault_target_iam_role_arn
-}
+# output "vault_target_iam_role_arn" {
+#   description = "The AWS IAM role ARN for the Vault assume role credential type."
+#   value       = module.aws_secrets.vault_target_iam_role_arn
+# }
 
 output "vault_role_iam_user_credential_type" {
   value       = module.aws_secrets.vault_role_iam_user_credential_type
   description = "The Vault role for AWS `iam_user` credential type."
 }
 
-output "vault_role_assumed_role_credential_type" {
-  value       = module.aws_secrets.vault_role_assumed_role_credential_type
-  description = "The Vault role for AWS `assumed_role` credential type."
-}
+# output "vault_role_assumed_role_credential_type" {
+#   value       = module.aws_secrets.vault_role_assumed_role_credential_type
+#   description = "The Vault role for AWS `assumed_role` credential type."
+# }
 
 output "vault_role_federation_token_credential_type" {
   value       = module.aws_secrets.vault_role_federation_token_credential_type
