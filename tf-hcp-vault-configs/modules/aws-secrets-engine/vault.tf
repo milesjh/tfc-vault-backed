5b10c72b0676c0ae80839cf1f972bcd95d48d3da
Miles <miles.harrison@hashicorp.com>
2023-04-24 10:09:48 -0500

    removing aws_backend username_template

--- tf-hcp-vault-configs/modules/aws-secrets-engine/vault.tf
+++ tf-hcp-vault-configs/modules/aws-secrets-engine/vault.tf
@@ -3,7 +3,7 @@ resource "vault_aws_secret_backend" "vault_aws" {
   secret_key        = aws_iam_access_key.vault_mount_user.secret
   description       = "AWS secrets engine"
   region            = data.aws_region.current.name
-  username_template = "{{ if (eq .Type \"STS\") }}{{ printf \"${aws_iam_user.vault_mount_user.name}-%s-%s\" (random 20) (unix_time) | truncate 32 }}{{ else }}{{ printf \"${aws_iam_user.vault_mount_user.name}-vault-%s-%s\" (unix_time) (random 20) | truncate 60 }}{{ end }}"
+  # username_template = "{{ if (eq .Type \"STS\") }}{{ printf \"${aws_iam_user.vault_mount_user.name}-%s-%s\" (random 20) (unix_time) | truncate 32 }}{{ else }}{{ printf \"${aws_iam_user.vault_mount_user.name}-vault-%s-%s\" (unix_time) (random 20) | truncate 60 }}{{ end }}"
 }
 
 resource "vault_aws_secret_backend_role" "vault_role_iam_user_credential_type" {
