e10436339f1ddf1a2613a882f077972a740becd6
Miles <miles.harrison@hashicorp.com>
2023-04-24 22:13:54 -0500

    Adding packer image reader role to dynamic azure creds

--- tf-hcp-vault-configs/modules/azure-secrets-engine/outputs.tf
+++ tf-hcp-vault-configs/modules/azure-secrets-engine/outputs.tf
@@ -1,4 +1,4 @@
-output "vault_azure_role" {
-  value       = vault_azure_secret_backend_role.role.role
+output "vault_azure_role1" {
+  value       = vault_azure_secret_backend_role.role1.role
   description = "The Vault role for Azure."
 }
