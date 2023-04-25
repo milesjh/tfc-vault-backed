baa03045972136857382c271218b1b159f0918ca
Miles <miles.harrison@hashicorp.com>
2023-04-25 02:26:33 -0500

    removing aws fed role

--- tf-hcp-vault-configs/modules/aws-secrets-engine/outputs.tf
+++ tf-hcp-vault-configs/modules/aws-secrets-engine/outputs.tf
@@ -3,7 +3,7 @@ output "vault_role_iam_user_credential_type" {
   description = "The Vault role for AWS `iam_user` credential type."
 }
 
-output "vault_role_federation_token_credential_type" {
-  value       = vault_aws_secret_backend_role.vault_role_federation_token_credential_type.name
-  description = "The Vault role for AWS `federation_token` credential type."
-}
+# output "vault_role_federation_token_credential_type" {
+#   value       = vault_aws_secret_backend_role.vault_role_federation_token_credential_type.name
+#   description = "The Vault role for AWS `federation_token` credential type."
+# }
--- tf-hcp-vault-configs/modules/aws-secrets-engine/vault.tf
+++ tf-hcp-vault-configs/modules/aws-secrets-engine/vault.tf
@@ -12,11 +12,11 @@ resource "vault_aws_secret_backend_role" "vault_role_iam_user_credential_type" {
   policy_document          = aws_iam_policy.vault_dynamic_iam_user_policy.policy
 }
 
-resource "vault_aws_secret_backend_role" "vault_role_federation_token_credential_type" {
-  backend         = vault_aws_secret_backend.vault_aws.path
-  credential_type = "federation_token"
-  name            = "vault-demo-federation-token"
-  policy_document = aws_iam_policy.vault_dynamic_iam_user_policy.policy
-}
+# resource "vault_aws_secret_backend_role" "vault_role_federation_token_credential_type" {
+#   backend         = vault_aws_secret_backend.vault_aws.path
+#   credential_type = "federation_token"
+#   name            = "vault-demo-federation-token"
+#   policy_document = aws_iam_policy.vault_dynamic_iam_user_policy.policy
+# }
 
 
