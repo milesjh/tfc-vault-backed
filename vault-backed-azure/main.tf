4bb14cda8e2ece5a5ed57fc9170eed280df6b040
Miles <miles.harrison@hashicorp.com>
2023-04-24 22:39:12 -0500

    Adding packer image reader role to dynamic azure creds

--- vault-backed-azure/main.tf
+++ vault-backed-azure/main.tf
@@ -56,11 +56,6 @@ resource "azurerm_virtual_network" "main" {
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
   tags = {
     environment = "sandbox"
   }
