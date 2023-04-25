c42fd136eee04b77361b87003f7d7b4472516b57
Miles <miles.harrison@hashicorp.com>
2023-04-24 21:31:39 -0500

    adding Azure VM from Packer

--- vault-backed-azure/main.tf
+++ vault-backed-azure/main.tf
@@ -71,7 +71,7 @@ resource "azurerm_virtual_network" "main" {
 resource "azurerm_subnet" "main" {
   name                 = "internal"
   resource_group_name  = data.azurerm_resource_group.main.name
-  virtual_network_name = data.azurerm_virtual_network.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
